5d0299be14b4008ae5949fec72705fd2e5c9590d
robsyr <robin.syring@gmx.de>
2024-08-19 11:20:06 +0200

    Update morphing.C

--- test/morphing.C
+++ test/morphing.C
@@ -1,85 +1,85 @@
 #include "RooRealVar.h"
+#include "RooRealVar.h"
+#include "RooWorkspace.h"
 #include "RooGaussian.h"
 #include "RooUniform.h"
 #include "RooDataSet.h"
 #include "RooPlot.h"
-#include "RooWorkspace.h"
-#include "RooMinimizer.h"
 #include "RooMomentMorphFuncND.h"
-#include "RooHistPdf.h"
-#include "RooFormulaVar.h"
-#include "RooMomentMorphFuncND.h"
-#include "TCanvas.h"
-#include "TLegend.h"
+#include "RooAbsPdf.h"
 
 using namespace RooFit;
 
-// Number of samples used to fill the histograms, adjust it to your needs
-const int n_samples = 100;
+// Number of samples to fill the histograms
+const int n_samples = 1000;
 
-// Function to perform morphing
-void morphing(RooWorkspace* workspace, int setting) {
-    // Get variables from the workspace
-    RooRealVar* x_var = workspace->var("x");
-    RooRealVar* mu_var = workspace->var("mu");
-    RooRealVar* sigma_var = workspace->var("sigma");
+// Define the morphing routine
+void perform_morphing(RooWorkspace *ws, RooMomentMorphFuncND::Setting setting)
+{
+    // Get Variables from the workspace
+    RooRealVar *x_var = ws->var("x");
+    RooRealVar *mu_var = ws->var("mu");
+    RooRealVar *sigma_var = ws->var("sigma");
+    RooAbsPdf *gauss = ws->pdf("gauss");
 
-    // Set up a frame for plotting
-    RooPlot* frame1 = x_var->frame();
+    // Initialize a plot
+    RooPlot *frame1 = x_var->frame();
 
     // Define binning for morphing
-    RooBinning bin_mu_x(4, 0.0, 4.0);
-    RooMomentMorphFuncND::Grid2 grid(bin_mu_x);
+    RooMomentMorphFuncND::Grid2 grid(RooBinning(4, 0.0, 4.0));
 
+    // Set binning of histograms, has to be customized for optimal results
     x_var->setBins(50);
 
-    // Number of 'sampled' gaussians, if you change it, adjust the binning properly
     std::vector<int> parampoints = {0, 1, 2, 3, 4};
 
-    for (auto i : parampoints) {
-        // Define the sampled Gaussians
+    for (auto i : parampoints)
+    {
+        // Define the sampled gaussians
         RooRealVar mu_help(Form("mu%d", i), Form("mu%d", i), i);
+        // Use * beacause RooGuassian expexts objects no pointers
         RooGaussian help(Form("g%d", i), Form("g%d", i), *x_var, mu_help, *sigma_var);
-        workspace->import(help);
+        ws->import(help);
 
-        // Fill the histograms
-        RooDataHist* hist = workspace->pdf(Form("g%d", i))->generateBinned(RooArgSet(*x_var), n_samples);
+        // Fill the histograms use a unique pointer to prevent memory leaks
+        std::unique_ptr<RooDataHist> hist1{dynamic_cast<RooDataHist *>(ws->pdf(Form("g%d", i))->generateBinned(*x_var, n_samples))};
 
-        // Make sure that every bin is filled and we don't get zero probability
-        for (int i_bin = 0; i_bin < hist->numEntries(); ++i_bin) {
-            hist->add(hist->get(i_bin), 1.0);
+        // Add the value 1 to each bin
+        for (int i_bin = 0; i_bin < hist1->numEntries(); ++i_bin)
+        {
+            const RooArgSet *binContent = hist1->get(i_bin);
+            hist1->add(*binContent, 1.0);
         }
 
-        // Add the PDF to the workspace, the intOrder of 1 is necessary for calculation of the NLL
-        workspace->import(RooHistPdf(Form("histpdf%d", i), Form("histpdf%d", i), RooArgSet(*x_var), *hist, 1));
+        // Add the pdf to the workspace
+        ws->import(RooHistPdf(Form("histpdf%d", i), Form("histpdf%d", i), *x_var, *hist1, 1));
 
-        // Add the PDF to the grid
-        grid.addPdf(*workspace->pdf(Form("histpdf%d", i)), i);
-
-        // For plotting purposes
-        workspace->pdf(Form("histpdf%d", i))->plotOn(frame1);
+        // Plot and add the pdf to the grid
+        RooAbsPdf *pdf = ws->pdf(Form("histpdf%d", i));
+        pdf->plotOn(frame1);
+        grid.addPdf(*pdf, i);
     }
 
-    // Create the morphing and add it to the workspace
-    RooMomentMorphFuncND morph_func("morph_func", "morph_func", RooArgList(*mu_var), RooArgList(*x_var), grid, setting);
+    // Create the morphing
+    RooMomentMorphFuncND morph_func("morpf_func", "morph_func", RooArgList(*mu_var), RooArgList(*x_var), grid, setting);
 
-    // Normalize the morphed object to be a PDF, set it false to prevent warning messages and computational speed up
-    morph_func.setPdfMode(false);
+    // Normalizing the morphed object to be a pdf, set it false to prevent warning messages and gain computational speed up
+    // morph_func.setPdfMode();
 
-    // Creating the morphed PDF
+    // Creating the morphed pdf
     RooWrapperPdf morph("morph", "morph", morph_func, true);
-    workspace->import(morph);
-    workspace->pdf("morph")->plotOn(frame1, LineColor(kRed));
+    ws->import(morph);
+    RooAbsPdf *morph_ = ws->pdf("morph");
+    morph_->plotOn(frame1, LineColor(kRed));
 
-    TCanvas* c0 = new TCanvas();
+    TCanvas *c0 = new TCanvas();
     frame1->Draw();
-
-    // Prevent the plot from being closed instantly
-    std::cin.get();
 }
 
-// Function to define the "observed" data in a workspace
-RooWorkspace* build_ws(double mu_observed) {
+// Define the workspace
+RooWorkspace *build_ws(double mu_observed)
+{
+    // Generate the 'observed' data
     RooRealVar x_var("x", "x", -5, 15);
     RooRealVar mu_var("mu", "mu", mu_observed, 0, 4);
     RooRealVar sigma_var("sigma", "sigma", 1.5);
@@ -87,64 +87,61 @@ RooWorkspace* build_ws(double mu_observed) {
     RooGaussian gauss("gauss", "gauss", x_var, mu_var, sigma_var);
     RooUniform uniform("uniform", "uniform", x_var);
 
-    RooDataSet* obs_data = gauss.generate(x_var, n_samples);
+    RooDataSet *obs_data = gauss.generate(x_var, n_samples);
     obs_data->SetName("obs_data");
 
-    // Using a workspace for easier processing inside the class
-    RooWorkspace* workspace = new RooWorkspace();
-    workspace->import(x_var);
-    workspace->import(mu_var);
-    workspace->import(sigma_var);
-    workspace->import(gauss);
-    workspace->import(uniform);
-    workspace->import(*obs_data);
+    // Using a workspace for easier processing
+    RooWorkspace *ws = new RooWorkspace();
+    ws->import(x_var);
+    ws->import(mu_var);
+    ws->import(sigma_var);
+    ws->import(gauss);
+    ws->import(uniform);
+    ws->import(*obs_data);
 
-    return workspace;
+    return ws;
 }
 
-void morphing_example() {
-    // The "observed" data
+// Do the example
+void morphing()
+{
+    // Define the 'observed' mu
     double mu_observed = 2.5;
-    RooWorkspace* workspace = build_ws(mu_observed);
 
-    RooRealVar* mu_var = workspace->var("mu");
-    RooRealVar* x_var = workspace->var("x");
-    RooRealVar* sigma_var = workspace->var("sigma");
-    RooAbsPdf* gauss = workspace->pdf("gauss");
-    RooAbsPdf* uniform = workspace->pdf("uniform");
-    RooDataSet* obs_data = (RooDataSet*)workspace->data("obs_data");
+    // Import variables from workspace
+    RooWorkspace *ws = build_ws(mu_observed);
 
-    // Compute the real likelihood ratio
-    RooFormulaVar llhr_calc("llhr_calc", "x[0] / (x[0] + x[1])", RooArgList(*gauss, *uniform));
+    perform_morphing(ws, RooMomentMorphFuncND::Linear);
 
-    // Create the exact negative log likelihood functions for Gaussian model
-    RooNLLVar* nll_gauss = gauss->createNLL(*obs_data);
+    RooRealVar *x_var = ws->var("x");
+    RooRealVar *mu_var = ws->var("mu");
+    RooRealVar *sigma_var = ws->var("sigma");
+    RooAbsPdf *gauss = ws->pdf("gauss");
+    RooAbsPdf *uniform = ws->pdf("uniform");
+    RooAbsData *obs_data = ws->data("obs_data");
 
-    // Compute the morphed NLL
-    morphing(workspace, RooMomentMorphFuncND::Linear);
-    RooNLLVar* nll_morph = workspace->pdf("morph")->createNLL(*obs_data);
+    // Create the exact negative log likelihood function for Gaussian model
+    RooAbsReal *nll_gauss = gauss->createNLL(*obs_data);
+
+    // Create the morphed negative log likelihood function
+    RooAbsReal *nll_morph = ws->pdf("morph")->createNLL(*obs_data);
 
     // Plot the negative logarithmic summed likelihood
-    TCanvas* c1 = new TCanvas();
-    RooPlot* frame = mu_var->frame(Title("SBI vs. Morphing"));
+    TCanvas *c1 = new TCanvas();
+    RooPlot *frame = mu_var->frame(Title("SBI vs. Morphing"));
     nll_gauss->plotOn(frame, LineColor(kBlue), ShiftToZero());
     nll_morph->plotOn(frame, LineColor(kRed), ShiftToZero());
     frame->Draw();
 
-    // Create a legend and add entries
-    TLegend* legend = new TLegend(0.7, 0.7, 0.9, 0.9);  // Adjust coordinates as needed
-    legend->AddEntry("gauss", "Gaussian", "l");
-    legend->AddEntry("morph", "Morphed", "l");
-    legend->Draw();
-
-    // Compute the minimum via Minuit and display the results
-    std::vector<RooNLLVar*> nlls = {nll_gauss, nll_morph};
-    for (auto nll : nlls) {
+    // Compute the minimum of the nll via minuit
+    std::vector<RooAbsReal *> nlls = { nll_gauss, nll_morph};
+    for (auto nll : nlls)
+    {
         RooMinimizer minimizer(*nll);
-        minimizer.setErrorLevel(0.5);  // Adjust the error level in the minimization to work with likelihoods
+        minimizer.setErrorLevel(0.5); 
         minimizer.setPrintLevel(-1);
         minimizer.minimize("Minuit2");
-        RooFitResult* result = minimizer.save();
+        RooFitResult *result = minimizer.save();
         result->Print();
     }
 }
